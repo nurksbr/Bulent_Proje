18dc68fc5983c3610345c4b6a13daa70a35bf80f
Fevziye Nur KESEBİR <147191768+nurksbr@users.noreply.github.com>
2025-06-12 23:39:31 +0300

    deneme

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -19,6 +19,7 @@ type MenuItem = {
     count: number
     color: string
   }
+  children?: MenuItem[]
 }
 
 export default function Sidebar() {
@@ -75,11 +76,11 @@ export default function Sidebar() {
   const menuGroups: MenuGroup[] = [
     {
       id: 'main',
-      title: 'Ana Menü',
+      title: 'Menü',
       items: [
         {
           id: 'dashboard',
-          title: 'Gösterge Paneli',
+          title: 'Dashboard',
           path: '/',
           icon: (
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -88,113 +89,54 @@ export default function Sidebar() {
           ),
         },
         {
-          id: 'products',
-          title: 'Ürünler',
-          path: '/products',
+          id: 'guvenlik',
+          title: 'Güvenlik Sistemleri',
+          path: '/guvenlik',
           icon: (
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
+              <path d="M2.003 5.884L10 2l7.997 3.884A2 2 0 0120 7.764V17a2 2 0 01-2 2H2a2 2 0 01-2-2V7.764a2 2 0 01.003-1.88zM10 4.236L4 6.618V17h12V6.618l-6-2.382z" />
             </svg>
           ),
-          badge: {
-            count: 12,
-            color: 'bg-blue-500'
-          }
+        },
+        {
+          id: 'roadblocker',
+          title: 'Road Blocker',
+          path: '/roadblocker',
+          icon: (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M4 3a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1H4zm1 2h10v10H5V5z" />
+            </svg>
+          ),
+        },
+        {
+          id: 'projeler',
+          title: 'Projeler',
+          path: '/projeler',
+          icon: (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v10H5V5zm2 2v2h2V7H7zm0 4v2h2v-2H7zm4-4v2h2V7h-2zm0 4v2h2v-2h-2z" />
+            </svg>
+          ),
+          children: [
+            {
+              id: 'satin-alim',
+              title: 'Satın Alım',
+              path: '/projeler/satin-alim',
+            },
+            {
+              id: 'pdf-yazdir',
+              title: 'PDF Yazdır',
+              path: '/projeler/pdf-yazdir',
+            },
+            {
+              id: 'fatura-olustur',
+              title: 'Fatura Oluştur',
+              path: '/projeler/fatura-olustur',
+            },
+          ],
         },
       ]
-    },
-    {
-      id: 'manage',
-      title: 'Yönetim',
-      items: [
-        {
-          id: 'pricing',
-          title: 'Fiyatlandırma',
-          path: '/pricing',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-            </svg>
-          ),
-        },
-        {
-          id: 'customers',
-          title: 'Müşteriler',
-          path: '/customers',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-            </svg>
-          ),
-        },
-        {
-          id: 'orders',
-          title: 'Siparişler',
-          path: '/orders',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-            </svg>
-          ),
-          badge: {
-            count: 5,
-            color: 'bg-green-500'
-          }
-        },
-      ]
-    },
-    {
-      id: 'reports',
-      title: 'Raporlar ve Analiz',
-      items: [
-        {
-          id: 'reports',
-          title: 'Raporlar',
-          path: '/reports',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
-          ),
-        },
-        {
-          id: 'analytics',
-          title: 'Analitik',
-          path: '/analytics',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm9 4a1 1 0 10-2 0v6a1 1 0 102 0V7zm-3 2a1 1 0 10-2 0v4a1 1 0 102 0V9zm-3 3a1 1 0 10-2 0v1a1 1 0 102 0v-1z" clipRule="evenodd" />
-            </svg>
-          ),
-        },
-      ]
-    },
-    {
-      id: 'settings',
-      title: 'Ayarlar',
-      items: [
-        {
-          id: 'settings',
-          title: 'Genel Ayarlar',
-          path: '/settings',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
-            </svg>
-          ),
-        },
-        {
-          id: 'users',
-          title: 'Kullanıcı Yönetimi',
-          path: '/users',
-          icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-            </svg>
-          ),
-        },
-      ]
-    },
+    }
   ]
 
   // Aktif menü öğesini kontrol et
